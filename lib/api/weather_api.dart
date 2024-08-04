3c0022ae1a8aef728154175b2f46ce6e8c881880
Heng-Bunleang <144595734+HB-Leang@users.noreply.github.com>
2024-08-04 21:54:58 +0700

    Update weather_api.dart

--- lib/api/weather_api.dart
+++ lib/api/weather_api.dart
@@ -1,6 +1,7 @@
 const weatherAPI =
     "https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}";
-const weatherAPIKey = "87e2ebf8117db6eaa0f7f712e76498c2";
-// const weatherAPIKey = "78fa1bf26e7928ac09ea6e1f586b5b10";
+const weatherAPIKey = "YOUR_API_KEY";
+
+// lat and lon of Cambodia
 const lat = 11.56;
 const lon = 104.93;
