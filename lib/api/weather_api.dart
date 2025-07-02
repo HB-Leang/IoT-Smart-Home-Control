ca50d269988bafee1c944cef0110c7d051a3a64b
HB-Leang <bunleang5289@gmail.com>
2025-07-02 22:23:20 +0700

    add weather_api.dart file

---
+++ lib/api/weather_api.dart
@@ -0,0 +1,7 @@
+const weatherAPI =
+    "https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}";
+const weatherAPIKey = "YOUR_API_KEY";
+
+// lat and lon of Cambodia
+const lat = 11.56;
+const lon = 104.93;
